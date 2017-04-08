9c683131f9f83f2d619483fb73d2e3dda55f639f
warglaive <warglaivegm@gmail.com>
2017-04-08 11:26:43 +0300

    LaptopUPdate

---
+++ 01.Odd Lines/01.Odd Lines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _01.Odd_Lines
+{
+   public class Program
+    {
+        public static void Main()
+        {
+            string[] text = File.ReadAllLines("input.txt");
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    File.AppendAllText("output.txt", text[i] + Environment.NewLine);
+                }
+            }
+            File.WriteAllLines("output.txt", text.Where((lines, index) => index % 2 == 1));
+        }
+    }
+}
