262ddc63e3f100ba5731138b81a3dc29bc5ab751
warglaivee <warglaivegm@gmail.com>
2017-04-11 18:53:25 +0300

    05. Folder Size written

---
+++ 05. Folder Size/05. Folder Size.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace _05.Folder_Size
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            string[] files = Directory.GetFiles("TestFolder");
+            double sum = 0;
+            foreach (var file in files)
+            {
+                var info = new FileInfo(file);
+                sum += info.Length;
+            }
+            sum = sum / 1024 / 1024;
+            File.WriteAllText("result.txt", sum.ToString());
+            Console.WriteLine(sum);
+        }
+    }
+}
