3f61078ac23f61155c32ca0e91d0a5d56eecbc9f
warglaivee <warglaivegm@gmail.com>
2017-04-11 17:49:30 +0300

    04. Merge Files working

---
+++ 04. Merge Files/04. Merge Files.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _04.Merge_Files
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var readFileOne = File.ReadAllLines("FileOne.txt");
+            var readFileTwo = File.ReadAllLines("FileTwo.txt");
+            for (int i = 0; i < readFileOne.Length; i++)
+            {
+                File.AppendAllText("result.txt", readFileOne[i] + Environment.NewLine);
+                File.AppendAllText("result.txt", readFileTwo[i] + Environment.NewLine);
+            }
+        }
+    }
+}
