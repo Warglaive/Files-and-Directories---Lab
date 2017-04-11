123a2ff991401108bfd23f0e5c4a4a5ec65a0d25
warglaivee <warglaivegm@gmail.com>
2017-04-11 17:18:17 +0300

    words Count PC update

--- 02. Line Numbers/02. Line Numbers.cs
+++ 02. Line Numbers/02. Line Numbers.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace _02.Line_Numbers
 {
-  public  class Program
+    public  class Program
     {
        public static void Main()
         {
---
+++ 03. Word Count/03. Word Count.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _03.Word_Count
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var words = File.ReadAllText("words.txt").ToLower().Split();
+            var text = File.ReadAllText("text.txt").ToLower()
+                .Split(new[] { ' ', ',', '-', '!', '?', '\n', '\r' }
+            , StringSplitOptions.RemoveEmptyEntries);
+            var wordsCount = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                wordsCount[word] = 0;
+            }
+            foreach (string word in text)
+            {
+                if (wordsCount.ContainsKey(word))
+                {
+                    wordsCount[word]++;
+                }
+            }
+            foreach (var item in wordsCount)
+            {
+                File.WriteAllLines("result.txt"
+                    , wordsCount.OrderByDescending(x => x.Value)
+                    .Select(x => x.Key + " - " + x.Value));
+            }
+        }
+    }
+}
